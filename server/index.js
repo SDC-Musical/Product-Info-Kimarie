ba74201587e8ee005e0cd31b5ad5e86245235652
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2020-11-14 13:16:41 -0800

    Add new lines to server/index.js to faciliating readability

--- server/index.js
+++ server/index.js
@@ -8,9 +8,11 @@ const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 3004;
 const app = express();
 const client = path.join(__dirname, '/../client/dist');
+
 mongoose.connect(`mongodb://${MONGO_HOST}/Product`)
   .then(() => console.log('Connected to MongoDB!'))
   .catch((err) => console.error("Coudn't connect MongoDB:", err));
+
 app.use((req, res, next) => {
   res.set({
     'Access-Control-Allow-Origin': '*',
@@ -18,16 +20,19 @@ app.use((req, res, next) => {
   });
   next();
 });
+
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', router);
 app.use('/products/:product_id', express.static(client));
 app.use('/', express.static(client));
+
 app.get('*', (req, res) => {
   if (Object.keys(req.params)[0] !== 'product_id') {
     res.status(404);
   }
   res.sendFile(path.join(__dirname, './../client/dist/index.html'));
 });
+
 app.listen(PORT, () => {
   console.log(`listening on port ${HOST}:${PORT}!`);
 });
