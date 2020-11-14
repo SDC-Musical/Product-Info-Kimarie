32460581a17d8dba441aba988725d24765f5131f
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2020-11-14 14:14:55 -0800

    Add create, update and delete routes to controller

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,14 @@ const controller = require('../controllers');
 router.get('/products/:product_id', controller.title);
 router.get('/brand/:brand', controller.brand);
 
+/* ----------------------- CRUD Additions-----*/
+// Create a new product
+router.post('/', controller.create);
+// Update a product
+router.put('/:product_id', controller.update);
+// Delete a product
+router.delete('/:product_id', controller.remove);
+
 /* ----------------------- External---------- */
 // router.get('reviews/product_id');
 
