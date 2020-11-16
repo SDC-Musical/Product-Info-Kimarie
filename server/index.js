8cba7aba551e1df38a9f6fbb85a496d93c3472a6
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2020-11-15 19:26:17 -0600

    Adds packages to potentially facilitate CRUD
    after troubleshooting basic routes

--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const router = require('./routes/index.js');
+//const parser = require('body-parser');
+//const cors = require('cors');
 const morgan = require('morgan');
 
 const MONGO_HOST = process.env.MONGO_HOST || 'localhost';
 const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 3004;
-const app = express();
+
 const client = path.join(__dirname, '/../client/dist');
 
-mongoose.connect(`mongodb://${MONGO_HOST}/Product`, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB!'))
-  .catch((err) => console.error("Coudn't connect MongoDB:", err));
-
+// Installed and using cors instead
 app.use((req, res, next) => {
   res.set({
     'Access-Control-Allow-Origin': '*',
@@ -22,11 +22,17 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.urlencoded({ extended: true }));
+//app.use(parser.urlencoded({ extended: true }));
+//app.use(parser.json());
+//app.use(cors());
 app.use(morgan('dev'));
 app.use('/api', router);
 app.use('/products/:product_id', express.static(client));
 app.use('/', express.static(client));
+
+mongoose.connect(`mongodb://${MONGO_HOST}/Product`, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB!'))
+  .catch((err) => console.error("Coudn't connect MongoDB:", err));
 
 app.get('*', (req, res) => {
   if (Object.keys(req.params)[0] !== 'product_id') {
