2efa76c6f2b71d2a6323b50cff4d94808c2aa580
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2020-12-10 12:42:58 -0700

    Pass 100 RPS with GET request

--- _test_/K6/script.js
+++ _test_/K6/script.js
@@ -3,9 +3,12 @@ import { check, sleep } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '30s', target: 10 },
-    //{ duration: '1m30s', target: 10 },
-    { duration: '30s', target: 0 },
+    { duration: '10s', target: 10 },
+    { duration: '20s', target: 25 },
+    { duration: '10s', target: 50 },
+    { duration: '30s', target: 75 },
+    { duration: '2m', target: 100 },
+    { duration: '1m30s', target: 0 },
   ],
 };
 
