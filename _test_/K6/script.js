14da6872f0b55b6e76bc4bf2404228817f89fb16
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2020-12-10 12:27:59 -0700

    Pass stress testing for 10 RPS

--- _test_/K6/script.js
+++ _test_/K6/script.js
@@ -3,9 +3,9 @@ import { check, sleep } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '30s', target: 1 },
+    { duration: '30s', target: 10 },
     //{ duration: '1m30s', target: 10 },
-    { duration: '20s', target: 0 },
+    { duration: '30s', target: 0 },
   ],
 };
 
