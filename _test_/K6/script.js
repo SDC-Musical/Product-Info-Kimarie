75780af303552c9477f2160f5c34aa7a2c3f1c7c
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2020-12-10 12:09:12 -0700

    Successfully run K6 stress test for 1 RPS

--- _test_/K6/script.js
+++ _test_/K6/script.js
@@ -1,56 +1,17 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { check, sleep } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '1m', target: 25 },
-    { duration: '2m', target: 50 },
-    { duration: '3m', target: 0 }
+    { duration: '30s', target: 1 },
+    //{ duration: '1m30s', target: 10 },
+    { duration: '20s', target: 0 },
   ],
 };
 
 export default function () {
-  const BASE_URL = 'https://localhost:3004/';
-
-  let responses = http.batch([
-    [
-      'GET',
-      `${BASE_URL}/api/products/1/`,
-      null,
-      { tags: { name: 'PublicCrocs' } },
-    ],
-    // [
-    //   'GET',
-    //   `${BASE_URL}/public/crocodiles/2/`,
-    //   null,
-    //   { tags: { name: 'PublicCrocs' } },
-    // ],
-    // [
-    //   'GET',
-    //   `${BASE_URL}/public/crocodiles/3/`,
-    //   null,
-    //   { tags: { name: 'PublicCrocs' } },
-    // ],
-    // [
-    //   'GET',
-    //   `${BASE_URL}/public/crocodiles/4/`,
-    //   null,
-    //   { tags: { name: 'PublicCrocs' } },
-    // ],
-  ]);
-
-  check(responses, { 'status was 200': (r) => r.status == 200 });
+  let res = http.get('http://localhost:3004/api/products/9888888');
+  check(res, { 'status was 200': (r) => r.status == 200 });
+  check(res, { 'status was 404': (r) => r.status == 404 });
   sleep(1);
 }
-
-/* Notes for extended testing as I scale up
-    { duration: '2m', target: 100 }, // below normal load
-    { duration: '5m', target: 100 },
-    { duration: '2m', target: 200 }, // normal load
-    { duration: '5m', target: 200 },
-    { duration: '2m', target: 300 }, // around the breaking point
-    { duration: '5m', target: 300 },
-    { duration: '2m', target: 400 }, // beyond the breaking point
-    { duration: '5m', target: 400 },
-    { duration: '10m', target: 0 }, // scale down. Recovery stage.
-    */
