8476b7cc66ea9c69e029b2e937def3bbd7d882a7
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2020-12-10 17:34:34 -0700

    CHange K6 script to single GET requset from batch

--- _test_/K6/script.js
+++ _test_/K6/script.js
@@ -16,37 +16,41 @@ export let options = {
 };
 
 export default function () {
-  const BASE_URL = 'http://localhost:3004';
-  let productId = 9699999;
-
-  let responses = http.batch([
-    [
-      'GET',
-      `${BASE_URL}/api/products/${++productId}`,
-      null,
-    ],
-    [
-      'GET',
-      `${BASE_URL}/api/products/${++productId}`,
-      null,
-    ],
-    [
-      'GET',
-      `${BASE_URL}/api/products/${++productId}`,
-      null,
-    ],
-    [
-      'GET',
-      `${BASE_URL}/api/products/${++productId}`,
-      null,
-    ],
-  ]);
-
-  const result = check(responses, {
-    'status is 200': (r) => r.status === 200,
+  let res = http.get('http://localhost:3004/api/products/9888888');
+  const result = check(res, {
+    'is status 200': (r) => r.status === 200,
   });
-
   sleep(1);
-
   errorRate.add(!result);
+
+  // const BASE_URL = 'http://localhost:3004';
+
+  // let responses = http.batch([
+  //   [
+  //     'GET',
+  //     `${BASE_URL}/api/products/9999999`,
+  //     null,
+  //   ],
+  //   [
+  //     'GET',
+  //     `${BASE_URL}/api/products/9899999`,
+  //     null,
+  //   ],
+  //   [
+  //     'GET',
+  //     `${BASE_URL}/api/products/9888888`,
+  //     null,
+  //   ],
+  //   [
+  //     'GET',
+  //     `${BASE_URL}/api/products/9959999`,
+  //     null,
+  //   ],
+  // ]);
+
+  // sleep(1);
+
+  // responses.forEach((response) => {
+  //   errorRate.add(!check(response, {'status is 200': (r) => r.status === 200,}));
+  // });
 }
